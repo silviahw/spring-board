44b76497dadb6c9683b5247ce508d107af679428
leeeunchae <polarbear4425@gmail.com>
2024-05-13 19:07:22 +0900

    first commit

---
+++ core-board/src/main/java/com/core/coreboard/CoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.core.coreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBoardApplication.class, args);
+	}
+
+}
